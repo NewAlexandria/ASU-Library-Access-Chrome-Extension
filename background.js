44178c58348fcb20f382c5f633a6915594a53a9d
Zachary Jones <zak@newalexandria.org>
2018-08-28 14:29:26 -0400

    update to prepend new resource prefix based on dashes

--- background.js
+++ background.js
@@ -5,6 +5,7 @@ var proxy_list = {
 function rewriteUrl(tab) {
   url_pieces     = tab.url.split('/');
   if (url_pieces[2].indexOf(proxy_list.arizona_state_univ.proxy) <= 0) {
+    url_pieces[1]  = url_pieces[1].replace('.','-');
     url_pieces[2] += proxy_list.arizona_state_univ.proxy;
     lib_url        = url_pieces .join('/');
     chrome.tabs.update(tab.id, {url:lib_url});
